8a3f4cb49b7ce455fbf25bdf77443d98c31453f9
Kamil <kamil.surma@wp.pl>
2019-01-12 12:58:09 +0100

    Zadania  z lekcji 4.

--- src/main/java/Lekcja4.java
+++ src/main/java/Lekcja4.java
@@ -3,41 +3,44 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Lekcja4 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        try (BufferedReader br = new BufferedReader(new FileReader("doskonale.txt")) ) {
-            String[] data;
-            String line;
-            int i;
-            while ((line = br.readLine()) != null) {
-                data = line.split("\\s+");
-                    for(String s : data) {
-                    i = Integer.parseInt(s);
-                    if(sumOfDividers(i)==i){
-                        System.out.println(i + " jest doskonala");
-                    }
+//        try (BufferedReader br = new BufferedReader(new FileReader("doskonale.txt"))) {
+//            String[] data;
+//            String line;
+//            int i;
+//            while ((line = br.readLine()) != null) {
+//                data = line.split("\\s+");
+//                for (String s : data) {
+//                    i = Integer.parseInt(s);
+//                    if (sumOfDividers(i) == i) {
+//                        System.out.println(i + " jest doskonala");
+//                    }
+//
+//                }
+//            }
+//
+//        } catch (Exception e) {
+//
+//            e.printStackTrace();
+//        }
 
-                }
-            }
-
-        }catch (Exception e) {
-
-            e.printStackTrace();
-        }
-
+        countWords();
 
     }
 
     public static int sumOfDividers(int n) {
-        int sum=0;
+        int sum = 0;
 
-        for (int i=1;i<=n/2; i++) {
-            if (n%i==0){
-                sum +=i;
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                sum += i;
             }
 
         }
@@ -48,7 +51,7 @@ public class Lekcja4 {
 
         try {
 
-            List<String> lines  = Files.readAllLines(Paths.get("doskonale.txt"));
+            List<String> lines = Files.readAllLines(Paths.get("doskonale.txt"));
 
 
         } catch (IOException e) {
@@ -57,3 +60,23 @@ public class Lekcja4 {
 
 
     }
+
+    public  static void countWords() throws IOException {
+
+        String fileContents = new String(Files.readAllBytes(Paths.get("tekst.txt")));
+        String[] data  = fileContents.split(",|\\.|\\(|\\)\\s+");
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : data) {
+            if (map.containsKey(s)) {
+                map.put(s, map.get(s) + 1);
+            } else {
+                map.put(s, 1);
+            }
+        }
+
+        map.forEach((k,v) -> {
+            System.out.println(k + "wystąpiło " + v + "razy");
+        });
+    }
+
+}
